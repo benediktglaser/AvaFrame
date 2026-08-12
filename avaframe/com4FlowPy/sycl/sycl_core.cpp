bdfcb00f43457ee0715d00627b03ab8f7c0bb5a4
benediktglaser <benediktglaser@protonmail.com>
2026-08-12 08:39:57 +0200

    fix(sycl): use direct host pointer binding in buffer constructors

--- avaframe/com4FlowPy/sycl/sycl_core.cpp
+++ avaframe/com4FlowPy/sycl/sycl_core.cpp
@@ -177,19 +177,19 @@ py::tuple run_sycl_calculation(
     float* forest_data = forest_ptr ? forest_ptr : dummy.data();
 
     if (num_release_cells > 0) {
-        sycl::buffer<int, 1>    buf_release_indices(release_flat_indices.begin(),   release_flat_indices.end());
-        sycl::buffer<float, 1>  buf_dem(dem_ptr,                                    dem_ptr + total_cells);
-        sycl::buffer<float, 1>  buf_z_delta(host_z_delta.begin(),                   host_z_delta.end());
-        sycl::buffer<float, 1>  buf_flux(host_flux.begin(),                         host_flux.end());
-        sycl::buffer<int, 1>    buf_counts(host_counts.begin(),                     host_counts.end());
+        sycl::buffer<int, 1>    buf_release_indices(release_flat_indices.data(),    sycl::range<1>(num_release_cells));
+        sycl::buffer<float, 1>  buf_dem(dem_ptr,                                    sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_z_delta(host_z_delta.data(),                    sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_flux(host_flux.data(),                          sycl::range<1>(total_cells));
+        sycl::buffer<int, 1>    buf_counts(host_counts.data(),                      sycl::range<1>(total_cells));
         
-        sycl::buffer<float, 1>  buf_var_alpha(var_alpha_data,                      var_alpha_data + total_cells);
-        sycl::buffer<float, 1>  buf_var_exponent(var_exponent_data,                var_exponent_data + total_cells);
-        sycl::buffer<float, 1>  buf_var_umax(var_umax_data,                        var_umax_data + total_cells);
-        sycl::buffer<float, 1>  buf_infra(infra_data,                              infra_data + total_cells);
-        sycl::buffer<float, 1>  buf_forest(forest_data,                            forest_data + total_cells);
-        sycl::buffer<float, 1>  buf_backcalc(host_backcalc.begin(),                 host_backcalc.end());
-        sycl::buffer<float, 1>  buf_forest_int(host_forest_int.begin(),             host_forest_int.end());
+        sycl::buffer<float, 1>  buf_var_alpha(var_alpha_data,                       sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_var_exponent(var_exponent_data,                 sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_var_umax(var_umax_data,                         sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_infra(infra_data,                               sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_forest(forest_data,                             sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_backcalc(host_backcalc.data(),                  sycl::range<1>(total_cells));
+        sycl::buffer<float, 1>  buf_forest_int(host_forest_int.data(),              sycl::range<1>(total_cells));
 
         q.submit([&](sycl::handler& cgh) {
             // Accessors
